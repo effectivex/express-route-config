616f9418b2dc5424365ca464123159a7fd8d6bc3
Łukasz Sentkiewicz <sirmims@gmail.com>
2018-03-13 15:10:03 +0100

    feat: init
    init

---
+++ test/express-route-config.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from '../src/express-route-config'
+
+/**
+ * Dummy test
+ */
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy()
+  })
+
+  it('DummyClass is instantiable', () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
